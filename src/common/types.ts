2e3dc4594b08fdf9bb6128bae221ba0097065a75
luongdev <luongld.it@gmail.com>
2025-03-19 11:05:17 +0000

    save point

--- src/common/types.ts
+++ src/common/types.ts
@@ -6,34 +6,12 @@ export type MessageType =
   | "CLIENT_DISCONNECT"
   | "PING"
   | "PONG"
-  | "ERROR"
-  | "COMMAND"
-  | "EVENT";
+  | "ERROR";
 
 export interface Message {
   type: MessageType;
   timestamp?: number;
   [key: string]: any;
-}
-
-export interface Command extends Message {
-  type: "COMMAND";
-  payload: CommandPayload;
-}
-
-export interface CommandPayload {
-  command: string;
-  data: any;
-}
-
-export interface Event extends Message {
-  type: "EVENT";
-  payload: EventPayload;
-}
-
-export interface EventPayload {
-  eventName: string;
-  data: any;
 }
 
 export interface InitializeResult {
