960597139b97a9bf4d1474b6219537c438ad971a
luongdev <luongld.it@gmail.com>
2025-03-19 10:23:33 +0000

    add event & command type

--- src/common/types.ts
+++ src/common/types.ts
@@ -6,12 +6,34 @@ export type MessageType =
   | "CLIENT_DISCONNECT"
   | "PING"
   | "PONG"
-  | "ERROR";
+  | "ERROR"
+  | "COMMAND"
+  | "EVENT";
 
 export interface Message {
   type: MessageType;
   timestamp?: number;
   [key: string]: any;
+}
+
+export interface Command extends Message {
+  type: "COMMAND";
+  payload: CommandPayload;
+}
+
+export interface CommandPayload {
+  command: string;
+  data: any;
+}
+
+export interface Event extends Message {
+  type: "EVENT";
+  payload: EventPayload;
+}
+
+export interface EventPayload {
+  eventName: string;
+  data: any;
 }
 
 export interface InitializeResult {
